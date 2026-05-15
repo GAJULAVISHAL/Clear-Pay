ee6d284f462d569d9dfae96b2e4ae79fed81451a
G.Vishal <vishalgajula0709@gmail.com>
2026-05-15 17:40:18 +0530

    pr

--- apps/user-app/lib/auth.ts
+++ apps/user-app/lib/auth.ts
@@ -3,6 +3,8 @@ import { betterAuth } from "better-auth"
 import { phoneNumber } from "better-auth/plugins"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 
+
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
